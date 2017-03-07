89e29e81c4cdbbfbf9727a830feda0eed3676133
Jon Kuperman <jon.kuperman@gmail.com>
2017-03-07 15:03:07 -0800

    preact example

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-console.log('hello')
+import { h, render } from 'preact'
+
+render((
+    <div id='foo'>
+        <span>Hello, world!</span>
+        <button onClick={ e => alert('hi!') }>Click Me</button>
+    </div>
+), document.body)
