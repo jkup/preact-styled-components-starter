1d1eedb1eda75527731cbe5ecef55fb0460274cc
Jon Kuperman <jon.kuperman@gmail.com>
2017-03-07 14:06:46 -0800

    change main app name

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/app.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'src/dist')
