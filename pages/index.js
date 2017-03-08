b513cb11d20ccdf707b8265af1cf819c237fd253
Jon Kuperman <jon.kuperman@gmail.com>
2017-03-07 18:29:50 -0800

    update content

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,37 @@
-import { h, render } from 'preact'
+import { render } from 'preact'
 
 import styled from 'styled-components'
 
-// Create a <Title> react component that renders an <h1> which is
-// centered, palevioletred and sized at 1.5em
 const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
-  color: palevioletred;
+  color: white;
 `;
 
-// Create a <Wrapper> react component that renders a <section> with
-// some padding and a papayawhip background
+const Content = styled.p`
+  font-size: 1em;
+  color: white;
+`;
+
 const Wrapper = styled.section`
   padding: 4em;
-  background: papayawhip;
+  background: #272830;
+`;
+
+const Button = styled.button`
+  background: #21ABC7;
+  color: white;
+  border: none;
+  border-radius: 25px;
+  padding: 5px 10px;
 `;
 
 export default () => (
   <div>
     <Wrapper>
-      <Title>Hello, world!</Title>
-      <button onClick={ e => alert('hi!') }>Click Me</button>
+      <Title>Node.js Tutorials</Title>
+      <Content>Learn node.js quickly with these free JavaScript and Node.js tutorials. Our courses cover Node.js, JavaScript and Web Performance!</Content>
+      <Button onClick={ e => console.log('hi!') }>Enroll Now</Button>
     </Wrapper>
   </div>
 )
